b51498b17423cfb6fea8b020bb84f183d8383ff4
Freddy <xoptimal@163.com>
2021-03-04 11:34:56 +0800

    constant data adjustment

--- packages/rem/src/index.tsx
+++ packages/rem/src/index.tsx
@@ -16,16 +16,24 @@ import Manual from './widget/Manual';
 import SimpleForm from './widget/SimpleForm';
 import ExTable from './widget/ExTable';
 import ExUpload from './widget/ExUpload';
-import type { ElementProps } from './layouts/ElementContainer';
+import type {ElementProps} from './layouts/ElementContainer';
 import ElementContainer from './layouts/ElementContainer';
 import type {
-  FloatActionType,
-  FloatLayoutProps,
-  FloatProps,
-  FloatType,
-} from './layouts/FloatLayout';
+    AuthorityRule,
+    BaseFieldType,
+    Field,
+    FormField,
+    IAuthority,
+    IRequest,
+    IValueEnum,
+    ProFieldRequestListData,
+    RequestOptions,
+    RequestResponse,
+    TableField,
+} from './interface';
+import type {FloatActionType, FloatLayoutProps, FloatProps, FloatType,} from './layouts/FloatLayout';
 import FloatLayout from './layouts/FloatLayout';
-import type { PageLayoutProps } from './layouts/PageLayout';
+import type {PageLayoutProps} from './layouts/PageLayout';
 import PageLayout from './layouts/PageLayout';
 
 import rem from './rem';
@@ -37,49 +45,51 @@ import useRequest from './hooks/useRequest';
 
 import './index.less';
 
-export type {
-  BaseFieldType,
-  AuthorityRule,
-  IAuthority,
-  RequestOptions,
-  IRequest,
-  ProFieldRequestListData,
-  IValueEnum,
-} from './interface';
 
 export type {
-  FloatType,
-  FloatLayoutProps,
-  FloatProps,
-  FloatActionType,
-  PageLayoutProps,
-  ElementProps,
+    Field,
+    TableField,
+    FormField,
+    BaseFieldType,
+    AuthorityRule,
+    IAuthority,
+    RequestOptions,
+    IRequest,
+    ProFieldRequestListData,
+    IValueEnum,
+    RequestResponse,
+    FloatType,
+    FloatLayoutProps,
+    FloatProps,
+    FloatActionType,
+    PageLayoutProps,
+    ElementProps,
 };
 
-export { useHandle, useRequest };
+export {useHandle, useRequest};
 
 export {
-  ElementContainer,
-  FloatLayout,
-  PageLayout,
-  ExCascader,
-  ExCheckbox,
-  ExDatePicker,
-  ExDrawer,
-  ExForm,
-  ExInputNumber,
-  ExInputText,
-  ExModal,
-  ExRadio,
-  ExSelect,
-  ExSkeleton,
-  ExSwitch,
-  ExTable,
-  ExTransfer,
-  ExTreeSelect,
-  ExUpload,
-  Manual,
-  SimpleForm,
+    ElementContainer,
+    FloatLayout,
+    PageLayout,
+    ExCascader,
+    ExCheckbox,
+    ExDatePicker,
+    ExDrawer,
+    ExForm,
+    ExInputNumber,
+    ExInputText,
+    ExModal,
+    ExRadio,
+    ExSelect,
+    ExSkeleton,
+    ExSwitch,
+    ExTable,
+    ExTransfer,
+    ExTreeSelect,
+    ExUpload,
+    Manual,
+    SimpleForm,
 };
 
 export default rem;
